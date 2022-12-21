636d6667576ee06685af571ee1ca0f745e1fce9a
urasfinks <urasfinks@yandex.ru>
2022-12-21 20:03:07 +0300

    Добавлено кеширование для HttpClient

--- src/main/java/ru/jamsys/virtual/file/system/view/FileViewKeyStore.java
+++ src/main/java/ru/jamsys/virtual/file/system/view/FileViewKeyStore.java
@@ -8,9 +8,13 @@ import ru.jamsys.virtual.file.system.File;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
+import javax.net.ssl.*;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.security.KeyStore;
+import java.security.SecureRandom;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 
 public class FileViewKeyStore implements FileView {
@@ -19,6 +23,13 @@ public class FileViewKeyStore implements FileView {
     private String securityKey;
     private Security security;
     private File file = null;
+    private final Map<String, SSLSocketFactory> sslSocketFactory = new ConcurrentHashMap<>();
+
+    @Getter
+    private KeyManager[] keyManagers;
+
+    @Getter
+    private TrustManager[] trustManagers;
 
     @Getter
     private volatile KeyStore keyStore = null;
@@ -45,6 +56,7 @@ public class FileViewKeyStore implements FileView {
                 e.printStackTrace();
             }
         }
+        sslSocketFactory.clear();
     }
 
     public void setSecret(String alias, String value) throws Exception {
@@ -65,6 +77,42 @@ public class FileViewKeyStore implements FileView {
         SecretKeyFactory factory = SecretKeyFactory.getInstance("PBE");
         PBEKeySpec keySpec = (PBEKeySpec) factory.getKeySpec(ske.getSecretKey(), PBEKeySpec.class);
         return new String(keySpec.getPassword());
+    }
+
+    public SSLSocketFactory getSslSocketFactory(String sslContextType) {
+        if (keyStore == null) {
+            return null;
+        }
+        String typeManager = "SunX509";
+        if (keyManagers == null) {
+            try {
+                String pass = security.get(securityKey);
+                KeyManagerFactory kmf = KeyManagerFactory.getInstance(typeManager);
+                kmf.init(keyStore, pass.toCharArray());
+                keyManagers = kmf.getKeyManagers();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        if (trustManagers == null) {
+            try {
+                TrustManagerFactory tmf = TrustManagerFactory.getInstance(typeManager);
+                tmf.init(keyStore);
+                trustManagers = tmf.getTrustManagers();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        if (!sslSocketFactory.containsKey(sslContextType)) {
+            try {
+                SSLContext ssl = SSLContext.getInstance(sslContextType);
+                ssl.init(keyManagers, trustManagers, new SecureRandom());
+                sslSocketFactory.put(sslContextType, ssl.getSocketFactory());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sslSocketFactory.get(sslContextType);
     }
 
 }
